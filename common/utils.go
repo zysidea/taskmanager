69938278f1e23920a924647eee8ad1ab23bb753f
zys <zysidea@gmail.com>
2017-03-11 22:13:59 +0800

    添加返回错误信息

---
+++ common/utils.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type (
+	appError struct {
+		Error      string `json:"error"`
+		Message    string `json:"message"`
+		HttpStatus int    `json:"status"`
+	}
+	errorResource struct {
+		Data appError `json:"data"`
+	}
+)
+
+func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
+	errObj := appError{
+		Error:      handlerError.Error(),
+		Message:    message,
+		HttpStatus: code,
+	}
+	log.Printf("AppError]: %s\n", handlerError)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	if j, err := json.Marshal(errorResource{Data: errObj}); err == nil {
+		w.Write(j)
+	}
+}

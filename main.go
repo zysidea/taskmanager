2942470fb9ea86ced1864501d06fbf5732ffcb13
zysidea <zysidea@gmail.com>
2017-03-17 19:18:03 +0800

    添加配置文件

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"taskmanager/common"
 	"taskmanager/routers"
+
 	"github.com/codegangsta/negroni"
-	"net/http"
-	"log"
 )
 
-func main()  {
+func main() {
 	common.StartUp()
-	router:=routers.InitRoutes()
-	n:=negroni.Classic()
+	router := routers.InitRoutes()
+	n := negroni.Classic()
 	n.UseHandler(router)
 
-	server:=&http.Server{
-		Addr:common.AppConfig.Server,
-		Handler:n,
+	server := &http.Server{
+		Addr:    common.AppConfig.Server,
+		Handler: n,
 	}
 	log.Println("Listening")
 	server.ListenAndServe()

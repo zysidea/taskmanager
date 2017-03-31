bdd9566321ef63753136dda8527fe38924492571
zysidea <zysidea@gmail.com>
2017-03-31 19:17:57 +0800

    完成note的controller

--- controllers/note_controller.go
+++ controllers/note_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2/bson"
 	"taskmanager/data"
+	"gopkg.in/mgo.v2"
 )
 
 //POST /notes
@@ -57,16 +58,76 @@ func UpdateNote(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-//GET notes
+//GET /notes
 func GetNotes(w http.ResponseWriter, r *http.Request) {
+	mc:=NewMongoContext()
+	defer mc.Close()
+	collection:=mc.GetCollection("notes")
+	repo:=&data.NoteRepository{collection}
+	notes:=repo.GetAllNotes()
+	if j,err:=json.Marshal(NotesResource{notes});err!=nil{
+		common.DisplayUnexpectedAppError(w, err)
+	}else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
 
+	}
 }
+//GEt /notes/{id}
 func GetNoteById(w http.ResponseWriter, r *http.Request) {
+	vars:=mux.Vars(r)
+	id:=vars["id"]
+	mc:=NewMongoContext()
+	defer mc.Close()
+	collection:=mc.GetCollection("notes")
+	repo:=&data.NoteRepository{collection}
+	note,err:=repo.GetNoteById(id)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		} else {
+			common.DisplayUnexpectedAppError(w, err)
+			return
+		}
+	}
+	if j,err:=json.Marshal(NoteResource{note});err!=nil{
+		common.DisplayUnexpectedAppError(w, err)
+	}else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
 
+	}
 }
+//GET /notes/tasks/{id}
 func GetNoteByTask(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	id := vars["id"]
+	mc := NewMongoContext()
+	collection := mc.GetCollection("notes")
+	repo := &data.NoteRepository{collection}
+	notes := repo.GetNoteByTask(id)
+	if j, err := json.Marshal(NotesResource{Data: notes}); err != nil {
+		common.DisplayUnexpectedAppError(w, err)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	}
 }
+//DELETE /notes/{id}
 func DeleteNote(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	id := vars["id"]
+	mc := NewMongoContext()
+	collection := mc.GetCollection("tasks")
+	repo := &data.NoteRepository{collection}
+	err := repo.DeleteNote(id)
+	if err != nil {
+		common.DisplayUnexpectedAppError(w, err)
+	} else {
+		w.WriteHeader(http.StatusNoContent)
+	}
 }
